769da1e13a48d8ac46fdda263b2f80a4ebf98c17
wang18 <wang18@uoregon.edu>
2017-10-13 11:11:54 -0700

    Create workouts page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { WorkoutsPage} from '../workouts/workouts';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
+  tab1Root = WorkoutsPage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
